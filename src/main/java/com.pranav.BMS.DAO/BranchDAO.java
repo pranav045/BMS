757cf8a6304d8cdebbd39c9883410282a4f8e242
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-27 19:13:45 +0530

    Create BranchDAO.java with features

---
+++ src/main/java/com.pranav.BMS.DAO/BranchDAO.java
@@ -0,0 +1,21 @@
+package com.pranav.BMS.DAO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.pranav.BMS.Entity.Branch;
+import com.pranav.BMS.Repository.BranchRepository;
+
+@Repository
+public class BranchDAO {
+	@Autowired
+	private BranchRepository branchRepository;
+
+	public Branch saveBranch(Branch branch) {
+		return branchRepository.save(branch);
+	}
+
+	public Branch getBranch(String id) {
+		return branchRepository.findById(id).orElse(null);
+	}
+}
