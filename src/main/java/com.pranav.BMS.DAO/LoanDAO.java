8c83f643df69ba50f5d420771671d62e405a5de8
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-28 11:54:08 +0530

    Create LoanDAO.java with the features to add and save new loan and get an existing loan with the id

---
+++ src/main/java/com.pranav.BMS.DAO/LoanDAO.java
@@ -0,0 +1,21 @@
+package com.pranav.BMS.DAO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.pranav.BMS.Entity.Loan;
+import com.pranav.BMS.Repository.LoanRepository;
+
+@Repository
+public class LoanDAO {
+	@Autowired
+	private LoanRepository loanRepository;
+
+	public Loan saveLoan(Loan loan) {
+		return loanRepository.save(loan);
+	}
+
+	public Loan getLoan(int id) {
+		return loanRepository.findById(id).orElse(null);
+	}
+}
