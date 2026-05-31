29cede7d9734f05ba25bb45e23c081d5eead5389
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-31 09:02:22 +0530

    Add BranchController with saveBranch endpoint

---
+++ src/main/java/com.pranav.BMS.Controller/BranchController.java
@@ -0,0 +1,22 @@
+package com.pranav.BMS.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pranav.BMS.DTO.ResponseStructure;
+import com.pranav.BMS.Entity.Branch;
+import com.pranav.BMS.Service.BranchService;
+
+@RestController
+public class BranchController {
+	@Autowired
+	private BranchService branchService;
+
+	@PostMapping("/saveBranch")
+	public ResponseEntity<ResponseStructure<Branch>> saveBranch(@RequestBody Branch branch) {
+		return branchService.saveBranch(branch);
+	}
+}
