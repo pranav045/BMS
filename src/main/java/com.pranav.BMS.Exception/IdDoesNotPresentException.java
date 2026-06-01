b084d8560914b7b25bc8cbd989816e2b1c1f7b13
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-06-01 08:19:17 +0530

    Create IdDoesNotPresentException class for handling id does not present exception

---
+++ src/main/java/com.pranav.BMS.Exception/IdDoesNotPresentException.java
@@ -0,0 +1,15 @@
+package com.pranav.BMS.Exception;
+
+public class IdDoesNotPresentException extends RuntimeException{
+	public String message;
+
+	public IdDoesNotPresentException() {
+	}
+
+	public IdDoesNotPresentException(String message) {
+		this.message = message;
+	}
+	public String toString() {
+		return message;
+	}
+}
