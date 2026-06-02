988106f2e77b854446447340c6d19ef6d7883ac0
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-06-02 11:07:54 +0530

    Add BankExceptionHandler for handling exceptions

---
+++ src/main/java/com.pranav.BMS.Exception/BankExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.pranav.BMS.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.pranav.BMS.DTO.ResponseStructure;
+
+public class BankExceptionHandler {
+	@ExceptionHandler(IdDoesNotPresentException.class)
+	public ResponseEntity<ResponseStructure<String>> catchException(IdDoesNotPresentException exception) {
+		ResponseStructure<String> rs = new ResponseStructure<String>();
+		rs.setData(exception.getMessage());
+		rs.setMessage("Bank not found");
+		rs.setStatusCode(HttpStatus.NOT_FOUND.value());
+		return new ResponseEntity<ResponseStructure<String>>(rs, HttpStatus.NOT_FOUND);
+	}
+}
