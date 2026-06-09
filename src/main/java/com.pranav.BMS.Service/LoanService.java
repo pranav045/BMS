e49a9761bebc26f1396c616744ff338fe7ef9e72
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-06-09 19:37:19 +0530

    Implement getLoan method with exception handling
    Added method to retrieve a loan by its ID, handling cases where the loan is not found.

--- src/main/java/com.pranav.BMS.Service/LoanService.java
+++ src/main/java/com.pranav.BMS.Service/LoanService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.pranav.BMS.DAO.LoanDAO;
 import com.pranav.BMS.DTO.ResponseStructure;
 import com.pranav.BMS.Entity.Loan;
+import com.pranav.BMS.Exception.IdDoesNotPresentException;
 
 @Service
 public class LoanService {
@@ -21,5 +22,18 @@ public class LoanService {
 		rs.setMessage("Loan added successfully");
 		rs.setStatusCode(HttpStatus.CREATED.value());
 		return new ResponseEntity<ResponseStructure<Loan>>(rs, HttpStatus.CREATED);
+	}
+
+	public ResponseEntity<ResponseStructure<Loan>> getLoan(int id) {
+		Loan data = loanDAO.getLoan(id);
+		if (data != null) {
+			ResponseStructure<Loan> rs = new ResponseStructure<Loan>();
+			rs.setData(data);
+			rs.setMessage("Loan id " + id + " found successfully");
+			rs.setStatusCode(HttpStatus.FOUND.value());
+			return new ResponseEntity<ResponseStructure<Loan>>(rs, HttpStatus.FOUND);
+		} else {
+			throw new IdDoesNotPresentException("Loan id " + id + " not found");
+		}
 	}
 }
