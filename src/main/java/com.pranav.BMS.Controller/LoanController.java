bf149c413eed146777ae5243d436f9f16b8e2002
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-06-10 11:39:20 +0530

    Add getLoan method and update saveLoan annotation

--- src/main/java/com.pranav.BMS.Controller/LoanController.java
+++ src/main/java/com.pranav.BMS.Controller/LoanController.java
@@ -2,7 +2,10 @@ package com.pranav.BMS.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pranav.BMS.DTO.ResponseStructure;
@@ -15,7 +18,12 @@ public class LoanController {
 	private LoanService loanService;
 
 	@PostMapping("/saveLoan")
-	public ResponseEntity<ResponseStructure<Loan>> saveLoan(Loan loan) {
+	public ResponseEntity<ResponseStructure<Loan>> saveLoan(@RequestBody Loan loan) {
 		return loanService.saveLoan(loan);
+	}
+
+	@GetMapping("/getLoan/{id}")
+	public ResponseEntity<ResponseStructure<Loan>> getLoan(@PathVariable int id) {
+		return loanService.getLoan(id);
 	}
 }
