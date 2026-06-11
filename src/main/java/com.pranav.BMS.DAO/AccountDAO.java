dadb4f84ef640f256c2c0da3f989dc5e0b3fd3b7
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-06-11 18:18:35 +0530

    Add method to retrieve all accounts

--- src/main/java/com.pranav.BMS.DAO/AccountDAO.java
+++ src/main/java/com.pranav.BMS.DAO/AccountDAO.java
@@ -1,5 +1,7 @@
 package com.pranav.BMS.DAO;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -17,5 +19,9 @@ public class AccountDAO {
 
 	public Account getAccount(String id) {
 		return accountRepository.findById(id).orElse(null);
+	}
+
+	public List<Account> getAllAccounts() {
+		return accountRepository.findAll();
 	}
 }
