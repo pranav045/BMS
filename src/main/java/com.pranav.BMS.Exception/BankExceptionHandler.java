2db2f24720bd76fe8c05d572d607e9a038a6d70b
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-06-16 17:49:54 +0530

    Add exception handler for EmptyException

--- src/main/java/com.pranav.BMS.Exception/BankExceptionHandler.java
+++ src/main/java/com.pranav.BMS.Exception/BankExceptionHandler.java
@@ -15,4 +15,13 @@ public class BankExceptionHandler {
 		rs.setStatusCode(HttpStatus.NOT_FOUND.value());
 		return new ResponseEntity<ResponseStructure<String>>(rs, HttpStatus.NOT_FOUND);
 	}
+
+	@ExceptionHandler(EmptyException.class)
+	public ResponseEntity<ResponseStructure<String>> catchEmptyEception(EmptyException exception) {
+		ResponseStructure<String> rs = new ResponseStructure<String>();
+		rs.setData(exception.getMessage());
+		rs.setMessage("No bank found");
+		rs.setStatusCode(HttpStatus.FOUND.value());
+		return new ResponseEntity<ResponseStructure<String>>(rs, HttpStatus.FOUND);
+	}
 }
