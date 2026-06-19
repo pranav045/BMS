b666dc51e73e4334ed898391afa4dd82dcff2683
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-06-19 18:20:18 +0530

    Add endpoint to retrieve all accounts

--- src/main/java/com.pranav.BMS.Controller/AccountController.java
+++ src/main/java/com.pranav.BMS.Controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.pranav.BMS.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,5 +27,10 @@ public class AccountController {
 	@GetMapping("/getAccount/{id}")
 	public ResponseEntity<ResponseStructure<Account>> getAccount(@PathVariable String id) {
 		return accountService.getAccount(id);
+	}
+
+	@GetMapping("/getAllAccounts")
+	public ResponseEntity<ResponseStructure<List<Account>>> getAllAccounts() {
+		return accountService.getAllAccounts();
 	}
 }
