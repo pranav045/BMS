a45dc46002f894ffdd1bf64976e8579f0fe3ae64
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-06-24 10:15:31 +0530

    Add deleteBank endpoint to BankController

--- src/main/java/com.pranav.BMS.Controller/BankController.java
+++ src/main/java/com.pranav.BMS.Controller/BankController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,5 +33,10 @@ public class BankController {
 	@GetMapping("/getAllBanks")
 	public ResponseEntity<ResponseStructure<List<Bank>>> getAllBanks() {
 		return bankService.getAllBanks();
+	}
+
+	@DeleteMapping("/deleteBank/{id}")
+	public ResponseEntity<ResponseStructure<Bank>> deleteBank(@PathVariable String id) {
+		return bankService.deleteBank(id);
 	}
 }
