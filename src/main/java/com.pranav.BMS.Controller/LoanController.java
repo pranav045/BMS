60778c7f83c3e8a6327b1c00073ec334d0eb13dc
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-06-25 21:12:24 +0530

    Add deleteLoan method to LoanController

--- src/main/java/com.pranav.BMS.Controller/LoanController.java
+++ src/main/java/com.pranav.BMS.Controller/LoanController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,5 +33,10 @@ public class LoanController {
 	@GetMapping("/getAllLoans")
 	public ResponseEntity<ResponseStructure<List<Loan>>> getAllLoans() {
 		return loanService.getAllLoans();
+	}
+
+	@DeleteMapping("/deleteLoan")
+	public ResponseEntity<ResponseStructure<Loan>> deleteLoan(@PathVariable int id) {
+		return loanService.deleteLoan(id);
 	}
 }
